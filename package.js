6885a91e8a573c2599787978ddd3fe1cba4cd828
Marcel Mellor <info@mellors.de>
2016-05-20 12:38:30 +0200

    new version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'mellors:agilecrm',
-    version: '0.0.1',
+    version: '0.0.2',
     // Brief, one-line summary of the package.
     summary: 'Insert and update contacts to agilecrm with the help of simple-schema and autoform',
     // URL to the Git repository containing the source code for this package.
